7e6c0423791f6ec6faf5b76c31b6469481cff20d
Sofia-Madryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-09-06 18:22:48 +0000

    Deploying to gh-pages from @ Sofia-Madryha/goit-js-hw-11@8711be3591e7c153593271b338a071c067461af2 🚀

---
+++ index.729d292f.js
@@ -0,0 +1,2 @@
+[document.querySelector(".search-form"),document.querySelector(".button-js")][0].addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget;console.log(t)}));
+//# sourceMappingURL=index.729d292f.js.map
---
+++ index.d8033fa6.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".search-form"),SubmitBtn:document.querySelector(".button-js")}).form.addEventListener("submit",(function(e){e.preventDefault();const t=e.currentTarget;console.log(t)}));
+//# sourceMappingURL=index.d8033fa6.js.map
