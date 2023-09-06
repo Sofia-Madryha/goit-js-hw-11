8711be3591e7c153593271b338a071c067461af2
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-09-06 21:22:23 +0300

    change package.json

--- src/js/index.js
+++ src/js/index.js
@@ -1,63 +1,15 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
 
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const breedSelect = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-
-(function addSelectOptions() {
-  loader.classList.remove('notvisible');
-  fetchBreeds()
-    .then(breeds => createMarkupSelect(breeds))
-    .catch(err => {
-      console.log(err);
-      Notify.failure('Oops, something went wrong!');
-    })
-    .finally(() => {
-      loader.classList.add('notvisible');
-      breedSelect.classList.remove('notvisible');
-    });
-})();
-
-breedSelect.addEventListener('change', handlerChange);
-
-function handlerChange(evt) {
-  loader.classList.remove('notvisible');
-  catInfo.innerHTML = ' ';
-  const breedId = evt.target.value;
-  fetchCatByBreed(breedId)
-    .then(cat => createMarkupInfo(cat))
-    .catch(err => {
-      console.log(err);
-      Notify.failure('Oops, something went wrong!');
-    })
-    .finally(() => {
-      loader.classList.add('notvisible');
-      // error.classList.add('notvisible');
-    });
+const elements = {
+    form: document.querySelector(".search-form"),
+    SubmitBtn: document.querySelector(".button-js")
+    
 }
 
-function createMarkupSelect(breeds) {
-  const markupBreeds = breeds
-    .map(breed => {
-      return `<option value="${breed.id}">${breed.name}</option>`;
-    })
-    .join('');
-  breedSelect.insertAdjacentHTML('beforeend', markupBreeds);
-}
+elements.form.addEventListener("submit", handlerSubmit);
 
-function createMarkupInfo(cat) {
-  const markupBreeds = cat
-    .map(cat => {
-      return `<div class="cat-info">
-      <div class="cat-container"> <img src="${cat.url}" alt="${cat.breeds[0].name}" class="img-cat"></div>
-      
-        <div class="cat-desc"> <h2>${cat.breeds[0].name}</h2>
-        <p>${cat.breeds[0].description}</p>
-        <p><b>Temperament:</b>${cat.breeds[0].temperament} </p></div>
-      </div>`;
-    })
-    .join('');
-  catInfo.innerHTML = markupBreeds;
+function handlerSubmit(evt){
+evt.preventDefault()
+const searchValue = evt.currentTarget;
+console.log(searchValue);
 }
