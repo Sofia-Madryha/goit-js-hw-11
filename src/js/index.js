ffe1497fbcb264f17b9e59adbd9031e2068f70f1
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-09-07 23:48:12 +0300

    add homework

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import { async } from '@vimeo/player';
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -6,22 +7,24 @@ const elements = {
   SubmitBtn: document.querySelector('.button-js'),
   inputValue: document.querySelector('input'),
   listGallery: document.querySelector('.gallery'),
+  loadBtn: document.querySelector('.load-more'),
 };
+let page = 1;
 
 elements.form.addEventListener('submit', handlerSubmit);
+elements.loadBtn.addEventListener('click', handlerLoadMore);
 
 async function handlerSubmit(evt) {
   evt.preventDefault();
   const searchValue = evt.currentTarget.searchQuery.value;
   const searchInfo = await serviceSearch(searchValue);
-  console.log(searchInfo);
-
-  elements.listGallery.innerHTML = createMarkup(searchInfo)
-
+  elements.listGallery.innerHTML = createMarkup(searchInfo);
+  elements.loadBtn.classList.remove('invisible');
 }
 
 async function serviceSearch(searchValue) {
   const BASE_URL = 'https://pixabay.com/api/';
+
   const params = new URLSearchParams({
     key: '39280260-fdc3cfd05b2bfaaf6335e84c8',
     q: searchValue,
@@ -29,7 +32,7 @@ async function serviceSearch(searchValue) {
     orientation: 'horizontal',
     safesearch: 'true',
     per_page: 40,
-    page: 1
+    page: page,
   });
 
   try {
@@ -49,6 +52,8 @@ async function serviceSearch(searchValue) {
           views,
           comments,
           downloads,
+          totalHits,
+          total,
         }) => {
           return {
             webformatURL,
@@ -58,6 +63,8 @@ async function serviceSearch(searchValue) {
             views,
             comments,
             downloads,
+            total,
+            totalHits,
           };
         }
       );
@@ -68,25 +75,52 @@ async function serviceSearch(searchValue) {
 }
 
 function createMarkup(data) {
-    return data.map(({webformatURL, tags, likes, comments, largeImageURL, downloads, views}) => {
+  return data
+    .map(
+      ({
+        webformatURL,
+        tags,
+        likes,
+        comments,
+        largeImageURL,
+        downloads,
+        views,
+      }) => {
         return `<div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" class="photo-img"  />
         <div class="info">
           <p class="info-item">
-            <b>Likes ${likes}</b>
+            <b>Likes <span class="numbers">${likes}</span></b>
           </p>
           <p class="info-item">
-            <b>Views ${views}</b>
+            <b>Views <span class="numbers">${views}</span></b>
           </p>
           <p class="info-item">
-            <b>Comments ${comments} </b>
+            <b>Comments <span class="numbers">${comments}</span> </b>
           </p>
           <p class="info-item">
-            <b>Downloads  ${downloads}</b>
+            <b>Downloads  <span class="numbers">${downloads}</span></b>
           </p>
         </div>
       </div>`;
-      })
-      .join('');
+      }
+    )
+    .join('');
+}
 
+async function handlerLoadMore() {
+  if (page >= 13) {
+    elements.loadBtn.classList.add('invisible');
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  } else {
+    page += 1;
+    searchValue = elements.inputValue.value;
+    const searchInfo = await serviceSearch(searchValue);
+    elements.listGallery.insertAdjacentHTML(
+      'beforeend',
+      createMarkup(searchInfo)
+    );
   }
+}
