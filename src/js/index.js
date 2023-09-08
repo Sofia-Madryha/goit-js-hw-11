f383c969295eb595d905bb0a8e32d4616be403bf
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-09-08 13:23:06 +0300

    add changes

--- src/js/index.js
+++ src/js/index.js
@@ -10,28 +10,33 @@ const elements = {
   loadBtn: document.querySelector('.load-more'),
 };
 
+let currentPage = 1;
 elements.form.addEventListener('submit', handlerSubmit);
 elements.loadBtn.addEventListener('click', handlerLoadMore);
 
 async function handlerSubmit(evt) {
   evt.preventDefault();
+  elements.loadBtn.classList.add('invisible');
+  elements.listGallery.innerHTML = '';
   const searchValue = evt.currentTarget.searchQuery.value;
   const searchInfo = await serviceSearch(searchValue);
+  
   elements.listGallery.innerHTML = createMarkup(searchInfo);
   elements.loadBtn.classList.remove('invisible');
+  currentPage=1;
 }
 
 
-async function handlerLoadMore(page) { 
-  page += 1;
-  if (page >= 13) {
-    elements.loadBtn.classList.add('invisible');
+async function handlerLoadMore() { 
+  currentPage += 1;
+    const searchValue = elements.inputValue.value;
+    const searchInfo = await serviceSearch(searchValue, currentPage);
+    if(searchInfo.length < 40){
+      elements.loadBtn.classList.add('invisible');
     Notify.failure(
       "We're sorry, but you've reached the end of search results."
     );
-  } else {
-    const searchValue = elements.inputValue.value;
-    const searchInfo = await serviceSearch(searchValue);
+    } else {
     elements.listGallery.insertAdjacentHTML(
       'beforeend',
       createMarkup(searchInfo)
--- src/js/service.js
+++ src/js/service.js
@@ -1,28 +1,39 @@
 import axios from 'axios';
-let page = 1;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+async function serviceSearch(searchValue, currentPage = 1) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '39280260-fdc3cfd05b2bfaaf6335e84c8',
+    q: searchValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 40,
+    page: currentPage,
+  });
 
-async function serviceSearch(searchValue) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const params = new URLSearchParams({
-      key: '39280260-fdc3cfd05b2bfaaf6335e84c8',
-      q: searchValue,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      per_page: 40,
-      page: page,
-    });
-  
-    try {
-      const response = await axios.get(`${BASE_URL}?${params}`);
-      if (response.data.hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        const data = response.data.hits;
-        return data.map(
-          ({
+  try {
+    const response = await axios.get(`${BASE_URL}?${params}`);
+    if (response.data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      page = currentPage;
+      const data = response.data.hits;
+      return data.map(
+        ({
+          webformatURL,
+          tags,
+          largeImageURL,
+          likes,
+          views,
+          comments,
+          downloads,
+          totalHits,
+          total,
+        }) => {
+          return {
             webformatURL,
             tags,
             largeImageURL,
@@ -30,27 +41,15 @@ async function serviceSearch(searchValue) {
             views,
             comments,
             downloads,
-            totalHits,
             total,
-          }) => {
-            return {
-              webformatURL,
-              tags,
-              largeImageURL,
-              likes,
-              views,
-              comments,
-              downloads,
-              total,
-              totalHits,
-            };
-          }
-        );
-      }
-    } catch (error) {
-      console.error(error);
+            totalHits,
+          };
+        }
+      );
     }
+  } catch (error) {
+    console.error(error);
   }
+}
 
-
-  export { serviceSearch}
+export { serviceSearch };
